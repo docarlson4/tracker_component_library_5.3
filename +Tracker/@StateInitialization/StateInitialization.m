31a295627ccf5f0f24c8f1913a2697cd695f9332
Douglas O. Carlson <doug.o.carlson@gmail.com>
2024-04-27 13:04:14 -0400

    completedd working version: One- and Two-Point init

--- +Tracker/@StateInitialization/StateInitialization.m
+++ +Tracker/@StateInitialization/StateInitialization.m
@@ -1,27 +1,62 @@
-classdef StateInitialization
-    %STATEINITIALIZATION Summary of this class goes here
-    %   Detailed explanation goes here
+classdef StateInitialization < handle
+    %STATEINITIALIZATION Handles various forms of state initialization
     
+    properties(Access=private)
+        expectedTypes = ["One-Point", "Two-Point"];
+    end
     properties
-        Type
+        Type    % One-Point, Two-Point
+        StateDim
+        VelMin
+        VelMax
+        AccMax
+        JrkMax
     end
     
     methods
-        function obj = StateInitialization(inputArg1,inputArg2)
-            %STATEINITIALIZATION Construct an instance of this class
-            %   Detailed explanation goes here
-            obj.Type = inputArg1 + inputArg2;
+        function obj = StateInitialization(varargin)
+            % STATEINITIALIZATION
+            DEFAULT.Type = "One-Point";
+            DEFAULT.StateDim = 6;
+            DEFAULT.VelMin = 0;
+            DEFAULT.VelMax = 100;
+            DEFAULT.AccMax = 10;
+            DEFAULT.JrkMax = 1;
 
+            p = inputParser;
+
+            cellfun( @(fn) p.addOptional( fn, DEFAULT.(fn) ), ...
+                fieldnames( DEFAULT ) );
+
+            p.parse(varargin{:});
+
+            param_names = fieldnames( p.Results );
+            for k = 1:numel(param_names)
+                obj.(param_names{k}) = p.Results.(param_names{k});
+            end
+            
             obj = init(obj);
         end
-        
-        function outputArg = method1(obj,inputArg)
-            %METHOD1 Summary of this method goes here
-            %   Detailed explanation goes here
-            outputArg = obj.Type + inputArg;
+
+        function [xNew, SNew] = Initialize(obj, tCur, zCur, SRCur)
+            switch obj.Type
+                case "One-Point"
+                    [xNew, SNew] = OnePoint(obj, zCur, SRCur);
+                case "Two-Point"
+                    [xNew, SNew] = TwoPoint(obj, tCur, zCur, SRCur);
+            end
         end
+    end
+
+    methods (Access = private)
+        [xNew, SNew] = OnePoint(obj, zCur, SRCur)
+        [xNew, SNew] = TwoPoint(obj, tCur, zCur, SRCur)
     end
 end
 
 function obj = init(obj)
+%INIT Validate inputs
+if ~any(contains(obj.Type, obj.expectedTypes))
+    error("Wrong Type. Expected " + strjoin(obj.expectedTypes, ", "))
+end
 end
