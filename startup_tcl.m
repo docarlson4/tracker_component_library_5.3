ed39baf37c0b7035d9aad28dda3facd560e13abf
Douglas O. Carlson <doug.o.carlson@gmail.com>
2025-02-07 20:26:36 -0500

    seup relevant directory structure

--- startup_tcl.m
+++ startup_tcl.m
@@ -2,11 +2,14 @@
 % clear
 % close all
 
-cd(fileparts(which(mfilename)))
-
 restoredefaultpath
 
-addpath( genpath(".") )
+addpath(genpath("C:/Users/dougo/source/repos/tracking/tracker_component_library_5.3/"))
+Constants;
+
+rmpath ./.git
+
+dbstop if error
 
 % Developed in Matlab 9.11.0.1769968 (R2021b) on PCWIN64.
 % Doug Carlson (doug.o.carlson@gmail.com), 2022-02-19 23:04
