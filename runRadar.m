e39c995bc143759d363af97b2660f2c3a53b61c6
Douglas O. Carlson <doug.o.carlson@gmail.com>
2026-03-19 17:30:07 -0400

    plot updates

--- runRadar.m
+++ runRadar.m
@@ -26,32 +26,41 @@ PRF = 1*kHz;
 
 tauP = 20/BW;
 
-% X-band LFM
 tx = Radar.RadarTransmitter(fc, Pt, tauP, PRF, ...
     'bandwidth', BW, ...
-    'waveform', 'lfm');
+    'waveform', 'barker');
 
-tx   % calls disp()
+tx;   % calls disp()
 
 Fs = 2*BW;
 Ts = 1/Fs;
 nFFT = 256;
 
 [s, t]  = tx.generatePulse(Fs);
-[Pout_dB, BW] = tx.spectrum(Fs, nFFT);
+[Pout_dB, f] = tx.spectrum(Fs, nFFT);
+
+figure
+plot(f/MHz, Pout_dB)
 
 h       = tx.matchedFilter(Fs);
 rc      = conv(s, h, 'full');    % range-compressed pulse
+figure
+plot(abs(rc))
 
 % % Barker-13
 % b13 = pi * [0 0 0 0 0 1 1 0 0 1 0 1 0]';
 % txB = Radar.RadarTransmitter(3e9, 10e3, 26e-6, 500, ...
 %     'waveform', 'barker', 'phaseCode', b13);
-[chi, tau_ax, fd_ax] = tx.ambiguityFunction(Fs, nFFT);
+[chi, tau_ax, fd_ax] = tx.ambiguityFunction(Fs);
 [FD, TAU] = meshgrid(fd_ax, tau_ax);
 
 %
 figure
-surf(TAU,FD,chi)
-shading flat
+surf(fd_ax*tauP, tau_ax/tauP, abs(chi))
+shading interp
 colorbar
+xlabel("\bfNormalized Doppler (F_D\tau)")
+ylabel("\bfNormalized Delay (t/\tau)")
+
+
+
